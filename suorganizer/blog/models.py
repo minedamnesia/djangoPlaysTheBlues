5ccd80610f32b0df7468ee098e4d7362660d2bec
minedamnesia <kyamamoto@mercycorps.org>
2016-05-29 00:37:40 -0700

    initial tut setup

---
+++ suorganizer/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(max_length=63, help_text='A label for URL config.', unique_for_month='pub_date'))
+    text = models.TextField()
+    pub_date = models.DateField('date published', auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startup = models.ManyToManyField(Startup, related_name='blog_posts')
---
+++ suorganizer/organizer/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Model Field Reference
+# https://docs.djangoproject.com/en/1.8/ref/models/fields
+
+class Tag(models.Model);
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config.')
+
+class Startup(models.Model);
+    name = models.CharField(max_length=31, db_index=True)
+    slug = models.SlugField(max_lenth=31, unique=True, help_text='A label for URL config.')
+    description = models.TextField()
+    founded_date = models.DateField('date founded')
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+class NewsLink(models.Model);
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField('date published')
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(Startup)
