4e6b2a9d96fcb859b1ee91be811f3a4569de9915
minedamnesia <minedamnesia@gmailcom>
2016-06-02 14:09:53 -0700

    demo through ch. 5

--- DjangoUnleashed/suorganizer/organizer/views.py
+++ DjangoUnleashed/suorganizer/organizer/views.py
@@ -1,9 +1,31 @@
 from django.http.response import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import (get_object_or_404, render)
 
-from .models import Tag
+from .models import Startup, Tag
 
-# Create your views here.
-def homepage(request):
-	tag_list = Tag.objects.all()
-	output = ", ".join([tag.name for tag in tag_list])
-	return HttpResponse(output)
+def startup_list(request):
+	return render(
+		request,
+		'organizer/startup_list.html',
+		{'startup_list': Startup.objects.all()})
+
+def startup_detail(request, slug):
+	startup = get_object_or_404(Startup, slug__iexact=slug)
+	return render(
+		request,
+		'organizer/startup_detail.html',
+		{'startup': startup})
+
+def tag_list(request):
+	return render(
+		request,
+		'organizer/tag_list.html',
+		{'tag_list': Tag.objects.all()})
+
+def tag_detail(request, slug):
+	tag = get_object_or_404(Tag, slug__iexact=slug)
+	return render(
+		request,
+		'organizer/tag_detail.html',
+		{'tag': tag})
